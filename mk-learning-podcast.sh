a586cd57c0c3c0c45b7e6bd3f1ff10870009993c
ry <sgmrrc@gmail.com>
2020-01-15 23:38:15 +0900

    5つコピーするように変更

--- mk-learning-podcast.sh
+++ mk-learning-podcast.sh
@@ -2,10 +2,22 @@
 # リストに書かれたファイルを順番にコピーする
 
 # パスとか
-f="../../Music/files/"
+f="../../Music/system/"
 t="../../Music/learning/"
 l="list.txt"
 
+# 下から2行め（2つ前）のファイルをコピーする
+h=`tail -n 2 $l | head -n 1`
+cp $f$h ${t}p2_$h
+# 下から7行め（7つ前）のファイルをコピーする
+h=`tail -n 7 $l | head -n 1`
+cp $f$h ${t}p7_$h
+# 下から24行め（24つ前）のファイルをコピーする
+h=`tail -n 24 $l | head -n 1`
+cp $f$h ${t}p24_$h
+# 下から79行め（79つ前）のファイルをコピーする
+h=`tail -n 79 $l | head -n 1`
+cp $f$h ${t}p79_$h
 # 1行目のファイルをコピーする
 h=`head -n 1 $l`
 cp $f$h $t
