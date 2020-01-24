5149f44ef7e0f758076b19239e4f2007cc070b00
sgmrrc <sgmrrc@gmail.com>
2020-01-24 20:51:22 +0900

    fibo

--- mk-learning-podcast.sh
+++ mk-learning-podcast.sh
@@ -8,8 +8,7 @@ l="list.txt"
 
 # 下からn行め（nつ前）のファイルをコピーする
 #ns="1 2 3 5 8 13 21 34 55 89 144 233 377 610"
-#ns="1 2 3 5 8"
-ns=`f=0 s=1; for ((;f+s<12;)); do t=$((f+s)) f=$s s=$t; echo -n $t\ ; done;`
+ns=`f=0 s=1; for ((;f+s<23;)); do t=$((f+s)) f=$s s=$t; echo -n $t\ ; done;`
 for n in $ns
 do
 	h=`tail -n $n $l | head -n 1`
